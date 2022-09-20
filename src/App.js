2861910349eb48cb01b459077f1b07389a21e2bf
Aikyn <ibrayevaiqyn@gmail.com>
2022-09-20 14:52:17 +0600

    modal

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Main from "./components/Main";
+import "./App.css";
+import Modal from "./components/modals/welcome/Modal";
+function App() {
+	return (
+		<div className="App">
+	
+		</div>
+	);
+}
+
+export default App;
