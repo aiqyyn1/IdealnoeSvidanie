0888aa477304a138eed3cd48ee8d079753e32e08
Aidyn Ibrayev <aidyn.nis12@gmail.com>
2022-09-20 15:16:28 +0600

    Revert "modal"

--- src/components/modals/welcome/index.js
+++ src/components/modals/welcome/index.js
@@ -1,34 +1,3 @@
-import React from 'react'
-import { useEffect } from 'react';
-export default function index() {
- 
-  const ref = useRef();
-  
-  useEffect(() => {
-    const check = (e) => {
-      if (ref.current && !ref.current.contains(e.target)) {
-        // onClose();
-      }
-    };
-    document.addEventListener('click', check);
-    return () => {
-      document.removeEventListener('click', check);
-    };
-  }, []);
-  return (
-    <div className='modal'>
-      <div className='modal-content' ref={ref}>
-        <div className='modal-tigutle'>
-          <strong>Modal Title</strong>
-        </div>
-        <div className='modal-body'>
-          <strong>Welcome to the our site!i</strong>
-          <br>s</br>
-        </div>
-        <div className='modal-footer'>
-          <button>Close</button>
-        </div>
-      </div>
-    </div>
-  );
-}
+import React from "react";
+
+export const WelcomeModal = () => <div></div>;
