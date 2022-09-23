9bcc23aa207d80d77fea10b328fd812399f25d3b
Aidyn Ibrayev <personal@NB11MDL004>
2022-09-24 03:33:03 +0600

    feat: start implementing main page with tailwind

--- src/components/common/select/index.js
+++ src/components/common/select/index.js
@@ -69,7 +69,7 @@ function classNames(...classes) {
 	return classes.filter(Boolean).join(" ");
 }
 
-export default function Select() {
+export default function Select({ options }) {
 	const [selected, setSelected] = useState(people[3]);
 
 	return (
--- src/components/main-form/index.js
+++ src/components/main-form/index.js
@@ -1,5 +1,14 @@
 import React from "react";
+import Select from "../common/select";
+import * as strings from "./strings";
 
 export default function MainForm() {
-	return <div>Form</div>;
+	return (
+		<div>
+			<div className="text-l font-semibold">{strings.TITLE}</div>
+			<div className="text-s">{strings.SUB_TITLE}</div>
+
+			<Select />
+		</div>
+	);
 }
